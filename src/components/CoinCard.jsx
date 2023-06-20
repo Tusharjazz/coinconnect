5b776a3d40e22fbcc76973c4888af405c1b58818
Tushar Ranjan <tushar.r@lemonaidegroup.com>
2023-06-20 10:54:49 +0700

    first commit

---
+++ src/components/CoinCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Heading, Text, VStack } from "@chakra-ui/react";
+
+const CoinCard = ({ id, name, img, symbol, price, currencySymbol = "฿" }) => (
+  <Link to={`/coin/${id}`}>
+    <VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <img src={img} w={"10"} h={"10"} objectfit={"contain"} alt={"Exchange"} />
+
+      <Heading size={"md"} noOfLines={1}>
+        {symbol}
+      </Heading>
+
+      <Text noOfLines={1}>{name}</Text>
+      <Text noOfLines={1}>{price ? `${currencySymbol}${price}` : "NA"}</Text>
+    </VStack>
+  </Link>
+);
+
+export default CoinCard;
